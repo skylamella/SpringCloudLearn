ae9875c1bb3a8492d1124d41a0a77927b0d64fe3
lamella <lamella@skyln.cn>
2022-08-27 22:42:39 +0800

    网关全局filter设置； nacos配置中心改造

---
+++ api-gateway/src/main/java/cn/skyln/gateway/filter/UserGlobalFilter.java
@@ -0,0 +1,34 @@
+package cn.skyln.gateway.filter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author: lamella
+ * @Date: 2022/08/27/17:27
+ * @Description:
+ */
+//@Component
+public class UserGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = exchange.getRequest().getHeaders().getFirst("token");
+        System.out.println(token);
+        if(StringUtils.isBlank(token)){
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
